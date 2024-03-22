4eba86dd79eddeeeeb81bfcb9309ce774b08e1a4
Guilherme Cazé <141370137+InfratorXD@users.noreply.github.com>
2024-03-22 10:47:04 -0300

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,39 @@
+package br.com.hospitalx.main;
+
+import br.com.hospitalx.veiculo.Carro;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Carro ferrari = new Carro("Ferrari", "SF90", 2024);
+		Carro uno = new Carro("Fiat Uno", "Sporting", 2003);
+
+		ferrari.acelerar();
+		uno.acelerar();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*Variaveis
+		
+		int numero1 = 10;
+		int numero2 = 10;
+		
+		int resultado = numero1 + numero2;
+		System.out.println(resultado);
+		
+		String nome = "Guilherme Cazé";
+		
+		System.out.println(nome);
+		*/
+		
+	}
+
+}
